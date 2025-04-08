50131e564bd70df9f51919824a975e35ee894b6f
teamprodevs <teamprodevs@gmail.com>
2025-04-08 05:31:59 +0000

    select products on order create

--- pages/Orders/jsobjects/order/order.js
+++ pages/Orders/jsobjects/order/order.js
@@ -1,5 +1,20 @@
 export default {
 	createProduct: async () => {
+		// Future logic for creating a product
 		return null;
+	},
+	getSelectProducts: () => {
+		return getAllProducts.data
+			.filter(p => p.ID)
+			.map(p => {
+			const isBundle = p.product_type === 'bundle';
+			const icon = isBundle ? '📦' : "" ;
+			const price = p.sale_price || p.regular_price || 'N/A';
+
+			return {
+				label: `${icon} ${p.post_title} (${price})`,
+				value: p.ID
+			};
+		});
 	}
 };
